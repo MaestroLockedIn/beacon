bdfbdbfc54586fd315fb62d10e8a035a797b1db2
Dominic Foli <codemaestro@outlook.com>
2026-02-03 20:51:05 +0100

    making progress

---
+++ src/scheduler/http_ping.rs
@@ -0,0 +1,57 @@
+use std::{collections::HashMap, error};
+
+use axum::Json;
+use reqwest::Response;
+use serde::Deserialize;
+use serde_json::Value;
+
+use crate::config::EndpointConfig;
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct HttpPing {
+    url: String,
+    rules: HttpRule,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct HttpRule {
+    is_successful: bool,
+}
+
+impl HttpPing {
+    pub fn new(config: &EndpointConfig) -> Self {
+        Self {
+            url: config.url.clone(),
+            rules: HttpRule {
+                is_successful: true,
+            },
+        }
+    }
+
+    pub async fn ping(&self) -> Result<(), Box<dyn error::Error>> {
+        let resp = reqwest::get(self.url.as_str()).await?;
+        let request_status = request_match(resp, &self.rules);
+        match request_status {
+            Ok(_) => {
+                println!("Ping Request is successful");
+                return Ok(());
+            }
+            Err(err) => {
+                println!("{}", err);
+                return Err(format!("{}", err).into());
+            }
+        }
+    }
+}
+
+fn request_match(resp: Response, rules: &HttpRule) -> Result<bool, &str> {
+    //TODO: if api response matches rule return true else return error
+
+    if rules.is_successful && !resp.status().is_success() {
+        return Err("Ping Request was not successful");
+    }
+
+    // I need to come up with other rules for http ping
+
+    Ok(true)
+}
--- src/scheduler/mod.rs
+++ src/scheduler/mod.rs
@@ -2,7 +2,9 @@ use std::sync::Arc;
 
 use tokio_cron_scheduler::{Job, JobScheduler};
 
-use crate::config::Config;
+use crate::{config::Config, scheduler::http_ping::HttpPing};
+
+pub mod http_ping;
 
 pub async fn run_tasks(config: Arc<Config>) {
     let sched = JobScheduler::new()
@@ -11,7 +13,7 @@ pub async fn run_tasks(config: Arc<Config>) {
     let job = Job::new_async("*/5 * * * * *", move |_, _| {
         let config = config.clone();
         Box::pin(async move {
-            scheduled_job(config);
+            scheduled_job(config).await;
         })
     })
     .expect("Failed to create job");
@@ -23,13 +25,17 @@ pub async fn run_tasks(config: Arc<Config>) {
     println!("Scheduler running in background.");
 }
 
-fn scheduled_job(config: Arc<Config>) {
-    //todo: ping the server to determine the health status
+async fn scheduled_job(config: Arc<Config>) {
     for c in &config.endpoints {
-        // println!("Running scheduled job for {:?}", c);
         println!(
             "Running scheduled job for {0} \n pinging {1}\n\n",
             c.name, c.url
         );
+        let req = HttpPing::new(&c);
+        let resp = req.ping().await;
+        match resp {
+            Ok(_) => println!("Request pinged successfully"),
+            Err(e) => println!("Error {:?}", e),
+        }
     }
 }
